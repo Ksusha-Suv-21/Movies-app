f549286ee8edcbe010ff14701a3c3598cf41928b
Ksusha Suvorova <ksuksu21bri@yandex.ru>
2024-12-07 14:40:54 +0300

    2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react'
 
 import { Layout, Space, Pagination } from 'antd'
 
+import MovieService from '../../services/movie_services'
 import MoviesList from '../Movies_list/Movies_list'
 import Header from '../Header/Header'
 
@@ -9,6 +10,8 @@ import './App.css'
 import 'antd/dist/reset.css'
 
 export default class App extends Component {
+  movieService = new MovieService()
+
   state = {
     error: null,
     isLoaded: true,
@@ -16,21 +19,21 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key=998cf5ec77bcc63d14e96455bc166802&s=return')
-      .then((res) => res.json())
+    this.movieService.searchMovies()
       .then(
         (data) => {
           this.setState({
             movies: data.results,
             isLoaded: false,
           })
-        },
-        (error) => {
-          this.setState({
-            isLoaded: false,
-            error,
-          })
         }
+      )
+      .catch((error) => {
+        this.setState({
+          isLoaded: false,
+          error,
+        })
+      }
       )
   }
 
