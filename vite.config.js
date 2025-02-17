197cea40e26988787a58588e296ebb8c1a03d802
Ksusha Suvorova <ksuksu21bri@yandex.ru>
2025-02-17 17:16:45 +0300

    dev

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Movies-app/'
+  base: 'https://ksusha-suv-21.github.io/Movies-app/'
 })
